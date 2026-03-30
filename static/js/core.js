9f58858a9ba477e6381393c2b7c69f8c66728e12
Shrinath Munagapati <112850938+MShrinath@users.noreply.github.com>
2026-03-31 01:15:41 +0530

    formatDomainName(domain) {
    remove calling favicon

--- static/js/core.js
+++ static/js/core.js
@@ -467,15 +467,9 @@ function escapeHTML(str) {
 }
 
 function formatDomainName(domain) {
-    // Try to fetch a favicon
-    return `
-        <div class="domain-display">
-            <div class="favicon-wrapper">
-                <img src="https://${domain}/favicon.ico" onerror="this.onerror=null;this.src='data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 width=%2216%22 height=%2216%22 viewBox=%220 0 24 24%22><path fill=%22%23aaa%22 d=%22M12 7V3H2v18h20V7H12zM6 19H4v-2h2v2zm0-4H4v-2h2v2zm0-4H4V9h2v2zm0-4H4V5h2v2zm4 12H8v-2h2v2zm0-4H8v-2h2v2zm0-4H8V9h2v2zm0-4H8V5h2v2zm10 12h-8v-2h2v-2h-2v-2h2v-2h-2V9h8v10zm-2-8h-2v2h2v-2zm0 4h-2v2h2v-2z%22/></svg>';" alt="${domain}">
-            </div>
-            <a href="https://${domain}" target="_blank" class="domain-link">${domain}</a>
-        </div>
-    `;
+    domain = domain.replace(/^https?:\/\//, '');
+    domain = domain.replace(/\/+$/, '');
+    return `<code>${escapeHTML(domain)}</code>`;
 }
 
 
